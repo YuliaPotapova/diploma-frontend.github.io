4291f5397897575f65a2b484279c784a4e41a576
Yulia Potapova <potapovaym@yandex.ru>
2020-07-02 17:41:21 +0400

    Этап 3. Инфраструктура и вёрстка - Webpack настроен и свёрстаны header и блок поиска

---
+++ babel.config.js
@@ -0,0 +1,17 @@
+﻿const presets = [
+  [
+    "@babel/env",
+    {
+      targets: [ // указать цели, для полифилов
+        "last 2 Chrome versions",
+        "last 2 ChromeAndroid versions",
+        "last 2 Firefox versions",
+        "last 2 Safari versions",
+        "Edge >= 15"
+      ],
+      useBuiltIns: "usage", // эта настройка babel-polyfill, если стоит значение usage, то будут подставлятся полифилы для версий браузеров которые указали выше.
+      corejs: "3.4.1" // явно проставить версию corejs
+    }
+  ]
+];
+module.exports = { presets };
